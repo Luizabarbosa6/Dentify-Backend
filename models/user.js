02aa74120984a282bee4f5325d3b3225cbcc2f0a
SanguinettiDev <bernardo.serrano.sanguinetti@gmail.com>
2025-04-09 17:14:35 -0300

    Adicionando Criptografia a Senha

--- models/user.js
+++ models/user.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema('User', {
-name: { type: String, required: true },
-email:{ type: String, required: true },
-password:{ type: String, required: true }
-
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
 });
 
+// Criptografa a senha antes de salvar
 userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require ('dotenv').config();
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
@@ -10,33 +10,30 @@ const app = express();
 app.use(express.json()); 
 app.use(cors());
 
-
-app.get('/', (rep,res) =>{
-    res.status(200).json({msg: 'Bem Vindo  API Dentify!'})
+app.get('/', (req, res) => {
+  res.status(200).json({ msg: 'Bem Vindo à API Dentify!' });
 });
 
 app.post('/', (req, res) => {  
-    const { name, email, password } = req.body 
+  const { name, email, password } = req.body;
 
-    if(!name) {
-        return res.status(422).json({ msg: 'O nome é obrigatório' })
-    }
-    if(!email) {
-        return res.status(422).json({ msg: 'O email é obrigatório' })
-    }
-    if(!password) {
-        return res.status(422).json({ msg: 'A senha é obrigatória' })
-    }
+  if (!name) {
+    return res.status(422).json({ msg: 'O nome é obrigatório' });
+  }
+  if (!email) {
+    return res.status(422).json({ msg: 'O email é obrigatório' });
+  }
+  if (!password) {
+    return res.status(422).json({ msg: 'A senha é obrigatória' });
+  }
 
-    
-    return res.status(200).json({ msg: 'Usuário validado com sucesso!' })
+  return res.status(200).json({ msg: 'Usuário validado com sucesso!' });
 });
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Banco Conectado!'))
   .catch(err => {
     console.error('Erro de conexão:', err.message);
-    
   });
 
-app.listen(3000)
+app.listen(3000);
