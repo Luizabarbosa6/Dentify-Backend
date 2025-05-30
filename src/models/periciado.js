13a58df58d77bcc3be7cf19e9178d149b3ee18d4
Luizabarbosa6 <153461981+Luizabarbosa6@users.noreply.github.com>
2025-05-29 22:06:10 -0300

    Atualização dashboard e periciado

--- src/models/periciado.js
+++ src/models/periciado.js
@@ -5,7 +5,7 @@ const periciadoSchema = new mongoose.Schema({
   caso: { type: mongoose.Schema.Types.ObjectId, ref: 'Case' },
   dataNascimento: { type: Date, required: true },
   sexo: { type: String, enum: ['Masculino', 'Feminino', 'Outro'], required: true },
-  cpf: { type: String, required: true },
+  cpf: { type: String, required: true, unique: true },
   nic: { type: String, required: true, unique: true },
   etnia: { type: String, enum: ['Preto', 'Pardo', 'Branco','Amarelo','Indígena'], required: true },
   criadoEm: { type: Date, default: Date.now }
